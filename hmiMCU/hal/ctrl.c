40face9cd622a6f94215e03b2a571c9911059781
IbrahimMohsenMoussa <ibrahim.moussa04@eng-st.cu.edu.eg>
2024-11-01 18:22:46 +0200

    ctrl interface

--- hmiMCU/hal/ctrl.c
+++ hmiMCU/hal/ctrl.c
@@ -6,3 +6,25 @@
  */
 
 #include "../mcal/uart.h"
+#include "ctrl.h"
+
+uint8 CTRL_init() {
+	uint8 l_byteRecieved = 0;
+	UART_ConfigType uartConfig = { .baudRate = 9600, .parity =
+			UART_PARITY_DISABLED, .stopBit = UART_STOP_BIT_1, .charSize =
+			UART_CHAR_SIZE_8, .asyncMode = LOGIC_LOW, .timeoutMs = 200
+
+	};
+	UART_init(&uartConfig);
+	for (int i = 0; i < 50; i++) {
+		UART_sendByte(CTRL_HANDSHAKE);
+
+		l_byteRecieved = (uint8) UART_recieveByte(l_byteRecieved);
+		if (l_byteRecieved == CTRL_HANDSHAKE)
+			return CTRL_CONNECTION_SUCSSES;
+	}
+return CTRL_CONNECTION_FAIL;
+}
+void CTRL_handShake() {
+
+}
--- hmiMCU/hal/ctrl.h
+++ hmiMCU/hal/ctrl.h
@@ -7,7 +7,9 @@
 
 #ifndef HAL_CTRL_H_
 #define HAL_CTRL_H_
-
+#define CTRL_HANDSHAKE 0x20
+#define CTRL_CONNECTION_SUCSSES 1
+#define CTRL_CONNECTION_FAIL 0
 
 
 #endif /* HAL_CTRL_H_ */
